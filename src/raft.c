054cb5aa0306a322ee72c7bf3ef90961286f8db0
Ace.Wu <ace.wu@yijinin.com>
2021-07-19 21:06:54 +0800

    test

--- src/raft.c
+++ src/raft.c
@@ -54,7 +54,6 @@ int raft_init(struct raft *r,
     r->election_timeout = DEFAULT_ELECTION_TIMEOUT;
     r->heartbeat_timeout = (int)((1 + rand() / (double)RAND_MAX) * 150);//(unsigned)r->io->random(r->io, (int)r->heartbeat_timeout,
                                                 //2 * (int)r->heartbeat_timeout);//DEFAULT_HEARTBEAT_TIMEOUT;
-    printf("heartbeat_timeout %d\n", r->heartbeat_timeout);
     r->install_snapshot_timeout = DEFAULT_INSTALL_SNAPSHOT_TIMEOUT;
     r->commit_index = 0;
     r->last_applied = 0;
